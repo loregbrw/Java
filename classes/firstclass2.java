616b1205073a28a80c096095cf1f5951242fa512
loregbrw <loregobara@gmail.com>
2024-01-04 12:07:21 -0300

    new code, print bytes

---
+++ classes/firstclass2.java
@@ -0,0 +1,27 @@
+import java.util.*; // importa tudo da util
+
+class Main {
+  public static void main(String[] args) {
+
+    byte[] bytes = new byte[256];
+
+    var rand = new Random();
+    rand.nextBytes(bytes);
+
+    int k = 0;
+
+    while (k < bytes.length) {
+      System.out.println(bytes[k]);
+      k++;
+    }
+    
+    if (bytes[0] == -128) {
+      System.out.println("Yep");
+    }
+    
+    for (int i = 0; i < bytes.length; i++) {
+      System.out.print(bytes[i] + " ");
+    }
+
+  }
+}
