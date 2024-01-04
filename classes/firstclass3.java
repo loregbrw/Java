93467d67a50d2c7d0d64189d29c82a2791c1beff
loregbrw <loregobara@gmail.com>
2024-01-04 12:07:21 -0300

    finished bytes code

--- classes/firstclass3.java
+++ classes/firstclass3.java
@@ -1,14 +1,23 @@
 import java.util.*; // importa tudo da util
 
-class firstclass3 {
+class Main {
   public static void main(String[] args) {
 
-    short[] testBytes = {173, 0b1010110, 0b01010110, 55, 200, 255};
-    compress(testBytes);
+    short[] testBytes = {173, 0b01010110, 0b01010110, 55, 200, 255};
 
     short[] compressedBytes = compress(testBytes);
+
+    short[] uncompressedBytes = uncompress(compressedBytes, 0);
     
-    // System.out.println(compress(testBytes));
+    for (int i = 0; i < testBytes.length; i++) {
+      System.out.println(testBytes[i]);
+    }
+
+    System.out.println();
+
+    for (int i = 0; i < uncompressedBytes.length; i++) {
+      System.out.println(uncompressedBytes[i]);
+    }
   }
   
   static short[] compress(short[] bytes) {
@@ -21,23 +30,23 @@ class firstclass3 {
       compressedBytes[i / 2] = (short)(first | second);
     }
 
-    for (int i = 0; i < compressedBytes.length; i++) {
-      System.out.println(compressedBytes[i]);
-    }
-
     return compressedBytes;
   }
 
-  static short[] uncompress(shrot[] bytes) {
+  static short[] uncompress(short[] bytes, int fillmode) {
     short[] uncompressedBytes = new short[ bytes.length * 2 ];
     int filled = 0;
-    
-    for (int i = 0; i < bytes.length; i+= 2) {
-      uncompressedBytes[i] = (short)(bytes[i] >> 4 << 4);
+    Random rand = new Random();
+
+    for (int i = 0; i < bytes.length; i++) {
+      int fillnumber = fillmode == 0 ? 0 : fillmode == 1 ? 7 : rand.nextInt((15 - 0) + 1) + 0;
+      
+      uncompressedBytes[filled] = (short)(bytes[i] >> 4 << 4 | fillnumber);
       filled++;
-      uncompressedBytes[i + 1] = (short)(bytes[i] >> 4); // error
+      uncompressedBytes[filled] = (short)((bytes[i] << 4) & 0b11111111 | fillnumber);
+      filled++;
     }
 
-    return uncompresseBytes;
+    return uncompressedBytes;
   }
 }
