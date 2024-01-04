919f62d10e25954f6a4786a1531e4c03ec81e137
loregbrw <loregobara@gmail.com>
2024-01-04 12:07:24 -0300

    merge sort logic

--- array_list/ArrayList.java
+++ array_list/ArrayList.java
@@ -1,3 +1,5 @@
+import java.util.Comparator;
+
 public class ArrayList<T> {
   private int count;
   private Object[] arr;
@@ -25,15 +27,15 @@ public class ArrayList<T> {
 
   public T get(int index) {
     if (index >= 0 && index < count)
-      return (T)arr[index];
-    
+      return (T) arr[index];
+
     throw new IndexOutOfBoundsException();
   }
 
   public void set(int index, T value) {
     if (index < 0 || index >= count)
       throw new IndexOutOfBoundsException();
-    
+
     arr[index] = value;
   }
 
@@ -66,26 +68,26 @@ public class ArrayList<T> {
 
   public ArrayList<T> clone() {
     ArrayList<T> copy = new ArrayList<T>();
-    
+
     copy.count = this.count;
     copy.arr = new Object[this.arr.length];
     for (int i = 0; i < count; i++)
       copy.arr[i] = this.arr[i];
-    
+
     return copy;
   }
-  
+
   public T remove(int index) {
     if (index < 0 || index >= count)
       throw new IndexOutOfBoundsException();
     var removedValue = arr[index];
 
     for (int i = index + 1; i < count; i++) {
-      arr[i - 1] = arr[i];  
+      arr[i - 1] = arr[i];
     }
     count--;
 
-    return (T)removedValue;
+    return (T) removedValue;
   }
 
   public void remove(T value) {
@@ -94,5 +96,60 @@ public class ArrayList<T> {
       return;
     remove(index);
   }
-  
+
+  public void sort(Comparator<T> cmp) {
+    Object[] temp = new Object[count];
+    mergeSort(cmp, arr, temp, 0, count);
+  }
+
+  private void mergeSort(
+      Comparator<T> cmp,
+      Object[] arr,
+      Object[] temp,
+      int start,
+      int end) {
+    if (end - start < 2)
+      return;
+
+    int middle = (start + end) / 2;
+    mergeSort(cmp, arr, temp, start, middle);
+    mergeSort(cmp, arr, temp, middle, end);
+    merge(cmp, arr, temp, start, middle, end);
+  }
+
+  private void merge(
+      Comparator<T> cmp,
+      Object[] arr,
+      Object[] temp,
+      int start,
+      int middle,
+      int end) {
+    int i = start, j = middle, k = start;
+    while (i < middle && j < end) {
+      var result = cmp.compare((T) arr[i], (T) arr[j]);
+      if (result < 0) {
+        temp[k] = arr[i];
+        i++;
+      } else {
+        temp[k] = arr[j];
+        j++;
+      }
+      k++;
+    }
+
+    while (i < middle) {
+      temp[k] = arr[i];
+      i++;
+      k++;
+    }
+
+    while (j < end) {
+      temp[k] = arr[j];
+      j++;
+      k++;
+    }
+
+    for (int m = start; m < end; m++)
+      arr[m] = temp[m];
+  }
 }
--- array_list/Main.java
+++ array_list/Main.java
@@ -1,9 +1,17 @@
 class Main {
   public static void main(String[] args) {
     var arr = new ArrayList<String>();
+    arr.add("World");
+    arr.add("Hello");
+    arr.add("Java");
+    arr.add("C#");
+    arr.add("Python");
+    
+    arr.sort((a, b) -> a.compareTo(b));
 
-    arr.add("Hello World");
-
-    System.out.println(arr.get(0));
+    for (int i = 0; i < arr.size(); i++)
+    {
+       System.out.println(arr.get(i)); 
+    }
   }
 }
