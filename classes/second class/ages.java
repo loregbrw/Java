0e8a55ad39c1a4db85f8b091564f754c184e3a2c
loregbrw <loregobara@gmail.com>
2024-01-04 12:07:22 -0300

    second class java

---
+++ classes/second class/ages.java
@@ -0,0 +1,48 @@
+public class Person {
+  private String login;
+  private String password;
+  private Integer age;
+
+  public String getLogin() {
+    return this.login;
+  }
+  public void setLogin(String value) {
+    this.login = value;
+  }
+
+  public String getPassword() {
+    return this.password;
+  }
+  public void setPassword(String value) {
+    this.password = value;
+  }
+
+  public Integer getAge() {
+    return this.age;
+  }
+  public void setAge(Integer value) {
+    this.age = value;
+  }
+  public void addAge(Integer value) {
+    this.age += value;
+  }
+}
+
+// =====================
+
+class Main {
+  public static void main(String[] args) {
+    Person person = new Person();
+
+    person.setLogin("login");
+    person.setPassword("password");
+    person.setAge(21);
+
+    System.out.println("Login: " + person.getLogin());
+    System.out.println("Password: " + person.getPassword());
+
+    person.addAge(1);
+
+    System.out.println("Age: " + person.getAge());
+  }
+}
