c634d7a0443b27b64de56a18e42bae74a69d05c6
loregbrw <loregobara@gmail.com>
2024-01-04 12:07:20 -0300

    firt code in java

---
+++ classes/firstclass.java
@@ -0,0 +1,10 @@
+import java.util.Scanner; // leitura de dados
+
+class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    System.out.println(2 * n); // printa o input * 2
+  }
+}
